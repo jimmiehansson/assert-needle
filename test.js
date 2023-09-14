3e0c70f884a7af3b22f0f429d5ddc57cca8801b5
Jimmie Hansson <jimmiehansson1985@gmail.com>
2023-09-14 09:10:48 +0200

    Refine tests

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ const { test } = require('tap');
 const { expected } = require('./index.js');
 
 test('test helper expected to receive string and list of elements to match', async ({ is }) => {
-  var str = '[1459875739796] WARN : pino.final with prettyPrint does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
+  var str = '[1459875739796] WARN : instance.final with output does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
   var s$ = [
-    'pino.final',
-    'prettyPrint',
+    'instance.final',
+    'output',
     'beforeExit',
     '123456',
     'abcdefghijklmnopqr',
@@ -17,10 +17,10 @@ test('test helper expected to receive string and list of elements to match', asy
   is(expected(str).output({ has: s$ }), true);
 });
 test('test helper expected to receive string and list of elements to not match', async ({ is }) => {
-  var str = '[1459875739796] WARN : pino.final with prettyPrint does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
+  var str = '[1459875739796] WARN : instance.final with output does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
   var s$ = [
-    '!pino.final',
-    '!prettyPrint',
+    '!instance.final',
+    '!output',
     '!beforeExit',
     '!123456',
     '!abcdefghijklmnopqr',
@@ -32,8 +32,8 @@ test('test helper expected to receive string and list of elements to not match',
 test('test helper expected to receieve empty value to not throw error', async ({ is }) => {
   var str = '';
   var s$ = [
-    'pino.final',
-    'prettyPrint',
+    'instance.final',
+    'output',
     'beforeExit',
     '123456',
     'abcdefghijklmnopqr',
@@ -43,7 +43,7 @@ test('test helper expected to receieve empty value to not throw error', async ({
   is(expected(str).output({ has: s$ }), undefined);
 });
 test('test helper expected to receieve non-array/list to not throw error', async ({ is }) => {
-  var str = '[1459875739796] WARN : pino.final with prettyPrint does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
+  var str = '[1459875739796] WARN : instance.final with output does not support flushing [1459875739796] INFO  (123456 on abcdefghijklmnopqr): beforeExit';
   var s$ = '';
   is(expected(str).output({ has: s$ }), undefined);
 });
